e2738271e6d5b5738b6c60243559226d71df0677
lazebird <lazebird@163.com>
2022-02-24 10:23:00 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,9 +19,28 @@ module.exports = defineConfig({
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     'no-irregular-whitespace': 'off',
-    'vue/multi-word-component-names': 'off',
-    'vue/require-default-prop': 'off',
-    'vue/max-attributes-per-line': 'off',
     '@typescript-eslint/no-var-requires': 'off',
+
+    'vue/multi-word-component-names': 'off',
+    'vue/attributes-order': 'off',
+    'vue/one-component-per-file': 'off',
+    'vue/html-closing-bracket-newline': 'off',
+    'vue/max-attributes-per-line': 'off',
+    'vue/multiline-html-element-content-newline': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/attribute-hyphenation': 'off',
+    'vue/require-default-prop': 'off',
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'always',
+          normal: 'never',
+          component: 'always',
+        },
+        svg: 'always',
+        math: 'always',
+      },
+    ],
   },
 });
